2573dab9cb09ed9e6e414a3d9dcb342a39411b81
linasedgwick <104169450+linasedgwick@users.noreply.github.com>
2022-05-02 14:54:30 +0200

    Added icons with respect to city

--- src/index.js
+++ src/index.js
@@ -32,12 +32,17 @@ function displayTemperature(response) {
   humidity.innerHTML = response.data.main.humidity;
   let windSpeed = document.querySelector("#wind-speed");
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
-  console.log(response);
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let icon = document.querySelector("#icon");
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  icon.setAttribute("alt", response.data.weather[0].description);
 }
 
+let city = "New York";
 let apiKey = "d505e69a528ffd38f9513a4e4c686175";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
